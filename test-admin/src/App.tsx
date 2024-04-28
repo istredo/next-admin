ec44b510df63ba182a43f84a75aa615636b02c55
istredo <uvarov6799@gmail.com>
2024-04-28 21:27:07 +0300

    init

---
+++ test-admin/src/App.tsx
@@ -0,0 +1,13 @@
+
+import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin';
+import { authProvider } from './authProvider';
+
+export const App = () => (
+    <Admin
+        	authProvider={authProvider}
+	>
+        
+    </Admin>
+);
+
+    
